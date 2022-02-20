53f4673c4e49d342cf909acdb9a2b3712e4c942f
Sacylkowski <sacylkowski@Sarahs-MBP.austin.rr.com>
2022-02-20 11:45:22 -0600

    fixed arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,8 @@
 // Assignment code here
-var upperCaseArray = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
-var lowerCaseArray = (a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z);
-var numericValuesArray = (1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
-var specialCharactersArray = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numericValuesArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var specialCharactersArray = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 
 // ADD GeneratePassword function
 function generatePassword() {
@@ -23,15 +23,13 @@ function generatePassword() {
 
   if (upperCase === "yes" || upperCase === "Yes" || upperCase === "YES") {
     window.alert("Password will have uppercase letters.");
-    var upperCaseArray = [Math.floor(Math.random() * upperCaseArray.length)];
+    // var upperCaseArray = [Math.floor(Math.random() * upperCaseArray.length)];
   }
   if (upperCase === "no" || upperCase === "No" || upperCase === "NO") {
     window.alert("Password will not have uppercase letters.")
-
   }
 
-  var lowerCase = prompt("Would you like to include lowercase letters?")
-  window.alert(lowerCase);
+  var lowerCase = window.prompt("Would you like to include lowercase letters?")
 
   if (lowerCase === "yes" || lowerCase === "Yes" || lowerCase === "YES") {
     console.log("Password will have lowercase letters.")
@@ -40,8 +38,7 @@ function generatePassword() {
     console.log("Password will not have lowercase letters.")
   }
 
-  var numericValues = prompt("Would you like to include numeric values?")
-  window.alert(numericValues);
+  var numericValues = window.prompt("Would you like to include numeric values?")
 
   if (numericValues === "yes" || numericValues === "Yes" || numericValues === "YES") {
     console.log("Password will have numbers.")
@@ -50,20 +47,18 @@ function generatePassword() {
     console.log("Password will not have numbers.")
   }
 
-
-
-
-  var specialCharacters = prompt("Would you like to include special characters?")
-  window.alert(specialCharacters);
+  var specialCharacters = window.prompt("Would you like to include special characters?")
 
   if (specialCharacters === "yes" || specialCharacters === "Yes" || specialCharacters === "YES") {
     window.alert("Password will have special characters.")
-    return specialCharactersArray[Math.floor(Math.random() * specialCharactersArray.length)];
+    // return specialCharactersArray[Math.floor(Math.random() * specialCharactersArray.length)];
   }
   if (specialCharacters === "no" || specialCharacters === "No" || specialCharacters === "NO") {
     window.alert("Password will not have special characters.")
 
   }
+
+  var password 
 }
 
 
